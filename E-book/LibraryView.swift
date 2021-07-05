63f3e06ef14dd4975f4afe27f760bd0d3a264020
minseongkim <kyrok67@naver.com>
2021-07-05 17:39:56 +0900

    Custom Segmented Control 구현.

---
+++ E-book/LibraryView.swift
@@ -0,0 +1,20 @@
+//
+//  LibraryView.swift
+//  E-book
+//
+//  Created by MIN SEONG KIM on 2021/07/05.
+//
+
+import UIKit
+
+class LibraryView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ E-book/ReadingBooksView.swift
@@ -0,0 +1,20 @@
+//
+//  ReadingBooksView.swift
+//  E-book
+//
+//  Created by MIN SEONG KIM on 2021/07/05.
+//
+
+import UIKit
+
+class ReadingBooksView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
